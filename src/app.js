6766d41596276266e6fc885e8440745c60279d9a
Bogdan Negura <id.bogdann@yahoo.com>
2019-08-15 22:10:02 +0100

    create the funtionality for removing items(cars) from local storage

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ const carInput = document.querySelector('#car');
 loadEventListners();
 
 function loadEventListners() {
+    // DOM Load Event
+    document.addEventListener('DOMContentLoaded', getCars);
     // Add car event
     form.addEventListener('submit', addCar);
     // Remove task event
@@ -17,6 +19,38 @@ function loadEventListners() {
     clearButton.addEventListener('click', clearCars);
     // Filter cars events
     filter.addEventListener('keyup', filterCars)
+}
+
+// Get cars from localStorage
+function getCars() {
+    let cars;
+    if(localStorage.getItem('cars') === null){
+        cars = [];
+    } else {
+        cars = JSON.parse(localStorage.getItem('cars'));
+    }
+
+    cars.forEach(function(cars){
+    // Create li element
+    const li = document.createElement('li');
+    // add class
+    li.className = 'collection-item';
+    // create text node and append to li
+    li.appendChild(document.createTextNode(cars));
+    // create new element
+    const link = document.createElement('a');
+    // add class
+    link.className = 'delete-item secondary-content';
+    // add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+
+    // append the link to li
+    li.appendChild(link);
+
+    // append to li to ul
+    carList.appendChild(li);
+
+    });
 }
 
 // Add Car
@@ -37,11 +71,16 @@ function addCar(e) {
     link.className = 'delete-item secondary-content';
     // add icon html
     link.innerHTML = '<i class="fa fa-remove"></i>';
+
     // append the link to li
     li.appendChild(link);
 
     // append to li to ul
     carList.appendChild(li);
+
+    // Store in Ls
+    storeCarInLocalStorage(carInput.value);
+
     // clear input
     carInput.value = '';
 
@@ -50,14 +89,50 @@ function addCar(e) {
     e.preventDefault();
 }
 
-// Remove Task
+// Store car
+
+function storeCarInLocalStorage(car) {
+    let cars;
+    if(localStorage.getItem('cars') === null){
+        cars = [];
+    } else {
+        cars = JSON.parse(localStorage.getItem('cars'));
+    }
+
+    cars.push(car);
+
+    localStorage.setItem('cars', JSON.stringify(cars));
+}
+
+// Remove Car
 function removeCar(e) {
     if(e.target.parentElement.classList.contains 
     ('delete-item')) {
         if(confirm('Are You Sure?')) {
             e.target.parentElement.parentElement.remove();
+
+            // Remove from LocalStorage
+            removeCarFormLocalStorage(e.target.parentElement.parentElement);
         }
     }
+}
+
+// Remove from LS
+function removeCarFormLocalStorage(carItem) {
+    let cars;
+    if(localStorage.getItem('cars') === null){
+        cars = [];
+    } else {
+        cars = JSON.parse(localStorage.getItem('cars'));
+    }
+
+    cars.forEach(function(car, index){
+        if(carItem.textContent === car){
+            cars.splice(index, 1);
+        }
+    });
+
+    localStorage.setItem('cars', JSON.stringify(cars));
 }
 
 // clear Cars
@@ -72,7 +147,18 @@ function clearCars() {
     }
 
     // https://jsperf.com/innerhtml-vs-removechild/47
+
+
+    // Clear from LS
+    clearCarsFromLocalStorage();
 }
+
+// Clear Cars from LS
+function clearCarsFromLocalStorage() {
+    localStorage.clear();
+}
+
+
 
 // Filter Cars
 function filterCars(e) {
